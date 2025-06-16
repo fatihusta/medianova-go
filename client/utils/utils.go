d7efa926500db17a80c6204ecd15927cdbe01964
Fatih USTA <fatihusta86@gmail.com>
2025-06-16 09:45:43 +0300

    added generic http request doer utility function
    Signed-off-by: Fatih USTA <fatihusta86@gmail.com>

--- client/utils/utils.go
+++ client/utils/utils.go
@@ -9,6 +9,30 @@ import (
 	"net/http"
 )
 
+func DoHTTPRequest[T any](c *http.Client, req *http.Request) (T, error) {
+
+	var result T
+
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("accept", "application/json")
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return result, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errMsg, err := ToStringBody(resp)
+		if err == nil {
+			return result, fmt.Errorf("request not succeeded. status:%d, error:%s", resp.StatusCode, errMsg)
+		}
+		return result, fmt.Errorf("request not succeeded. status:%d", resp.StatusCode)
+	}
+
+	return FromJSONToStruct[T](resp)
+}
+
 func FromJSONToStruct[T any](resp *http.Response) (T, error) {
 	var response T
 	if resp.Body == nil {
